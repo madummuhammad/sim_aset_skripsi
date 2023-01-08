e0864963217ad1f19d31edaf033a4c3374f0a09a
madummuhammad <muhammad.madum2018@gmail.com>
2023-01-08 15:15:58 +0700

    no urut

--- resources/views/showasset.blade.php
+++ resources/views/showasset.blade.php
@@ -16,6 +16,7 @@
                         </div>
                         <div class="col-sm-6 p-md-0 justify-content-sm-end mt-2 mt-sm-0 d-flex">
                             <ol class="breadcrumb">
+                                <li class="breadcrumb-item active"><a href="{{url('asset')}}">Aset</a></li>
                                 <li class="breadcrumb-item active"><a href="javascript:void(0)">Edit</a></li>
                             </ol>
                         </div>
